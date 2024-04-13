91628e3ad102d5d616bb5100ec71fad3d6d41d4e
Atharv <atharvpathak248@gmail.com>
2024-04-13 12:29:54 +0530

    Center the social icons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,51 +15,53 @@ const About = () => {
       <p className='text-gray-500 text-lg md:text-xl lg:text-base xl:text-xl dark:text-gray-400'>
         {description}
       </p>
-      <div className='social-links'>
-        <a
-          href='https://www.linkedin.com/in/pathak-ji-op'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={LinkedInIcon}
-            alt='LinkedIn'
-            className='social-icon'
-          />
-        </a>
-        <a
-          href='https://twitter.com/atharvpathak248'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={TwitterIcon}
-            alt='Twitter'
-            className='social-icon'
-          />
-        </a>
-        <a
-          href='https://github.com/pathakjiop'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={GitHubIcon}
-            alt='GitHub'
-            className='social-icon'
-          />
-        </a>
-        <a
-          href='https://www.instagram.com/pathak_ji_op'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={InstagramIcon}
-            alt='Instagram'
-            className='social-icon'
-          />
-        </a>
+      <div className=''>
+        <div className='social-links'>
+          <a
+            href='https://www.linkedin.com/in/pathak-ji-op'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={LinkedInIcon}
+              alt='LinkedIn'
+              className='social-icon'
+            />
+          </a>
+          <a
+            href='https://twitter.com/atharvpathak248'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={TwitterIcon}
+              alt='Twitter'
+              className='social-icon'
+            />
+          </a>
+          <a
+            href='https://github.com/pathakjiop'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={GitHubIcon}
+              alt='GitHub'
+              className='social-icon'
+            />
+          </a>
+          <a
+            href='https://www.instagram.com/pathak_ji_op'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={InstagramIcon}
+              alt='Instagram'
+              className='social-icon'
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
